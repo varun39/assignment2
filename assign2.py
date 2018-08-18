89b25e1fe437e7fad21efd07d99d1fba36907eb0
varun39 <v.goyal39@gmail.com>
2018-08-19 00:54:37 +0530

    assignment2

---
+++ assign2.py
@@ -0,0 +1,24 @@
+#  ASSIGNMENT 2
+#  q-1 PRINT SOMETHING
+print("Hello World")
+#  q-2 CONCATINATE STRINGS
+a="Acad"
+b="view"
+print(a+b)
+#  q-3 PRINT 3 USER DEFINED VARIABLES
+a=input("Enter value of a :")
+b=input("Enter value of b :")
+c=input("Enter value of c :")
+print(a,b,c)
+# q-4 Print Let's Get Started
+print("Let's Get Started")
+# q-5 Print the Given Values Using Placeholders
+s="Acadview"
+course="Python" 
+fees=5000
+print("%s %s %d"%(s,course,fees))
+# q-6 Find the Area of Circle With User Defined Radius
+pi=3.14
+r=int(input("Enter radius of circle :"))
+area=pi*r*r
+print("area of circles is ",area)
